7c0aa448ba76f24cb51781f7fa24f35f96d3d9c1
Priyanka Valesha <priyankavalesha14@gmail.com>
2023-12-31 17:31:30 +0530

    structure modified

--- src/stories/button.stories.ts
+++ src/stories/button.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 
-import { ButtonComponent } from './button.component';
+import { ButtonComponent } from '../components/button/button.component';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta: Meta<ButtonComponent> = {
